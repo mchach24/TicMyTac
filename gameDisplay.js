100d46593801f3eb25a35ccd587f999dfd89f9f1
mchach24 <hach24mc@gmail.com>
2015-05-14 21:17:14 -0400

    Added drawSubBoard function

--- gameDisplay.js
+++ gameDisplay.js
@@ -1,11 +1,19 @@
 function drawSubBoard(subGame) {
 	var context = subGame.getContext('2d');
 	context.beginPath();
-	context.moveTo(subGame.width/3,subGame.height/3);
-	context.lineTo(subGame.width/1.33,subGame.height/4);
-	context.moveTo(subGame.width/4,subGame.height/1.33);
-	context.lineTo(subGame.width/1.33,subGame.height/1.33);
-	context.lineWidth = subGame.width/30;
-    context.strokeStyle = '#cc1111';
+	context.moveTo(subGame.width/30,subGame.height/3);
+	context.lineTo(subGame.width/1.034,subGame.height/3);
+	
+	context.moveTo(subGame.width/30,subGame.height/1.5);
+	context.lineTo(subGame.width/1.034,subGame.height/1.5);
+	
+	context.moveTo(subGame.width/3,subGame.height/30);
+	context.lineTo(subGame.width/3,subGame.height/1.034);
+	
+	context.moveTo(subGame.width/1.5,subGame.height/30);
+	context.lineTo(subGame.width/1.5,subGame.width/1.034)
+	
+	context.lineWidth = subGame.width/60;
+    context.strokeStyle = '#000000';
 	context.stroke();
 }
