efc457d03147064c9a6213314aa62a6cb4e61187
mchach24 <hach24mc@gmail.com>
2015-05-16 23:23:08 -0400

    Added conditions for which square of subGame user clicked using click coordinates.

--- mainGame.js
+++ mainGame.js
@@ -10,9 +10,9 @@ subGames.push(new subGame(7, document.getElementById('f32')));
 subGames.push(new subGame(8, document.getElementById('f33')));
 var turn = 'X';
 var gameOver = false;
-while (!gameOver) {}
 
-function turnHandler(event, id, canvas) {
+function turnHandler(event, id, subGame) {
+	var position;
 	var x;
 	var y;
 	if (event.pageX || event.pageY) {
@@ -23,7 +23,36 @@ function turnHandler(event, id, canvas) {
 		x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
 		y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
 	}
-	x -= canvas.offsetLeft;
-	y -= canvas.offsetTop;
-	alert(x + "," + y);
+	x -= subGame.offsetLeft;
+	y -= subGame.offsetTop;
+	//alert(x + "," + y);
+	if (x > 0 && y > 0 && x < subGame.width/3 && y < subGame.height/3) {
+		position = 0;
+	}
+	else if (x > subGame.width/3 && y > 0 && x < subGame.width/1.5 && y < subGame.height/3) {
+		position = 1;
+	}
+	else if( x > subGame.width/1.5 && y > 0 && x < subGame.width && y < subGame.height/3) {
+		position = 2;
+	}
+	else if (x > 0 && y > subGame.height/3 && x < subGame.width/3 && y < subGame.height/1.5) {
+		position = 3;
+	}
+	else if (x > subGame.width/3 && y > subGame.height/3 && x < subGame.width/1.5 && y < subGame.height/1.5) {
+		position = 4;
+	}
+	else if (x > subGame.width/1.5 && y > subGame.height/3 && x < subGame.width && y < subGame.height/1.5) {
+		position = 5;
+	}
+	else if (x > 0 && y > subGame.height/1.5 && x < subGame.width/3 && y < subGame.height) {
+		position = 6;
+	}
+	else if (x > subGame.width/3 && y > subGame.height/1.5 && x < subGame.width/1.5 && y < subGame.height) {
+		position = 7;
+	}
+	else if (x > subGame.width/1.5 && y > subGame.height/1.5 && x < subGame.width && y < subGame.height) {
+		position = 8;
+	}
+	alert(position);
+	//markSquare(subGame, position, turn);
 }
